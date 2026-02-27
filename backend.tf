6655b7e61f041b31a46dc4649fd3d93b4bd92ebf
tdops darock <tolaoredope@gmail.com>
2026-02-27 00:45:35 +0000

    Remove Pluralsight-related documentation

--- backend.tf
+++ backend.tf
@@ -8,10 +8,10 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stterraformstate"
-    container_name       = "tfstate"
-    key                  = "vwan/terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "rg-terraform-state"
+  #   storage_account_name = "stterraformstate"
+  #   container_name       = "tfstate"
+  #   key                  = "vwan/terraform.tfstate"
+  # }
 }
